a2f3539cbc047335fb01934f3ae923ef4adda519
geoff <mac@macs-MacBook-Pro.local>
2020-09-15 22:45:16 +0300

    update for rest api

--- src/main/java/com/springinit/demo/DemoApplication.java
+++ src/main/java/com/springinit/demo/DemoApplication.java
@@ -4,14 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
+		// ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
 
-		for (String name : applicationContext.getBeanDefinitionNames()) {
-			System.out.println(name);
-		}
+		// for (String name : applicationContext.getBeanDefinitionNames()) {
+		// 	System.out.println(name);
+		// }
 	}
 
 }
