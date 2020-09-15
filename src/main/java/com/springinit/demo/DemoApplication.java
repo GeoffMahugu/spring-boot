9c9e53e195c44bf3d045577335648e4661226d07
geoff <mac@macs-MacBook-Pro.local>
2020-09-15 22:30:04 +0300

    Updated log for bean

--- src/main/java/com/springinit/demo/DemoApplication.java
+++ src/main/java/com/springinit/demo/DemoApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+
+		for (String name : applicationContext.getBeanDefinitionNames()) {
+			System.out.println(name);
+		}
 	}
 
 }
