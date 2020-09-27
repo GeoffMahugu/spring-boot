13086a81fea9ba2ef836eddfc474978f001af73c
geoff <mac@macs-MacBook-Pro.local>
2020-09-27 19:31:25 +0300

    Updated configs

--- src/main/java/com/springinit/demo/yarn/HelloPojo.java
+++ src/main/java/com/springinit/demo/yarn/HelloPojo.java
@@ -6,14 +6,19 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FsShell;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.yarn.annotation.OnContainerStart;
+import org.springframework.yarn.annotation.YarnComponent;
 
+
+@YarnComponent
 public class HelloPojo {
     private static final Log log = LogFactory.getLog(HelloPojo.class);
 
     @Autowired
     private Configuration configuration;
 
-    @OnYarnContainerStart
+//    @OnYarnContainerStart
+    @OnContainerStart
     public void publicVoidNoArgsMethod() {
         log.info("Hello from HelloPojo");
         log.info("About to list from hdfs root content");
@@ -21,5 +26,6 @@ public class HelloPojo {
         for (FileStatus s : shell.ls(false, "/")) {
             log.info(s);
         }
-//    }
+    }
+
 }
