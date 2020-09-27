b4dca982e4b378a69f88e9fd5bedc237fb271af1
geoff <mac@macs-MacBook-Pro.local>
2020-09-27 04:18:16 +0300

    Added application configs file

--- src/main/java/com/springinit/demo/yarn/HelloPojo.java
+++ src/main/java/com/springinit/demo/yarn/HelloPojo.java
@@ -4,21 +4,20 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.yarn.config.annotation.ContainerActivatorAnnotationPostProcessor
 
 public class HelloPojo {
     private static final Log log = LogFactory.getLog(HelloPojo.class);
 
     @Autowired
-    private Configuration configuration;
+//    private Configuration configuration;
 
-    @OnYarnContainerStart
-    public void publicVoidNoArgsMethod() {
-        log.info("Hello from HelloPojo");
-        log.info("About to list from hdfs root content");
-        FsShell shell = new FsShell(configuration);
-        for (FileStatus s : shell.ls(false, "/")) {
-            log.info(s);
-        }
-    }
+//    @OnYarnContainerStart
+//    public void publicVoidNoArgsMethod() {
+//        log.info("Hello from HelloPojo");
+//        log.info("About to list from hdfs root content");
+//        FsShell shell = new FsShell(configuration);
+//        for (FileStatus s : shell.ls(false, "/")) {
+//            log.info(s);
+//        }
+//    }
 }
