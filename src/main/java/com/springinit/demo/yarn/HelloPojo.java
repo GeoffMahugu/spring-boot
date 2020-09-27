b7cb2d42dea9fde9bfbe21e4326d7c9b03a79e6b
geoff <mac@macs-MacBook-Pro.local>
2020-09-27 04:29:29 +0300

    Removed old dependancies

--- src/main/java/com/springinit/demo/yarn/HelloPojo.java
+++ src/main/java/com/springinit/demo/yarn/HelloPojo.java
@@ -2,8 +2,12 @@ package com.springinit.demo.yarn;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.data.hadoop.fs.FsShell;
+import org.springframework.yarn.annotation.OnContainerStart;
+import org.springframework.yarn.annotation.YarnComponent;
 
 public class HelloPojo {
     private static final Log log = LogFactory.getLog(HelloPojo.class);
