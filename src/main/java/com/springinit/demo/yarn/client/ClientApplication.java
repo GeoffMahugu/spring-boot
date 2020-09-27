ee78654b45655d5e0928a1c84af29dc659ecf73b
geoff <mac@macs-MacBook-Pro.local>
2020-09-27 20:11:06 +0300

    Client config done

--- src/main/java/com/springinit/demo/yarn/client/ClientApplication.java
+++ src/main/java/com/springinit/demo/yarn/client/ClientApplication.java
@@ -1,4 +1,16 @@
 package com.springinit.demo.yarn.client;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.yarn.client.YarnClient;
+
+@EnableAutoConfiguration
 public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args)
+                .getBean(YarnClient.class)
+                .submitApplication();
+    }
+
 }
